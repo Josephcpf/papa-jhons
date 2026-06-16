5c193d1f500e935feb2a9a4493ec2e6acfac7de7
Josephcpf <joseph.cabanillas@utec.edu.pe>
2026-06-16 18:03:52 -0500

    Add files via upload

---
+++ src/orders/updateOrderStatus.py
@@ -0,0 +1,73 @@
+import json
+import boto3
+from datetime import datetime
+
+from src.shared.dynamodb import (
+    ORDERS_TABLE,
+    HISTORY_TABLE
+)
+
+events = boto3.client("events")
+
+
+def handler(event, context):
+
+    order_id = event["pathParameters"]["orderId"]
+
+    body = json.loads(event["body"])
+
+    new_status = body["status"]
+
+    timestamp = datetime.utcnow().isoformat()
+
+    # Actualizar pedido
+    ORDERS_TABLE.update_item(
+        Key={
+            "tenant_id": "PAPAJOHNS",
+            "order_id": order_id
+        },
+        UpdateExpression="""
+            SET #status = :status,
+                updated_at = :updated_at
+        """,
+        ExpressionAttributeNames={
+            "#status": "status"
+        },
+        ExpressionAttributeValues={
+            ":status": new_status,
+            ":updated_at": timestamp
+        }
+    )
+
+    # Guardar historial
+    HISTORY_TABLE.put_item(
+        Item={
+            "order_id": order_id,
+            "event_time": timestamp,
+            "status": new_status
+        }
+    )
+
+    # Publicar evento en EventBridge
+    events.put_events(
+        Entries=[
+            {
+                "Source": "orders.service",
+                "DetailType": "OrderStatusChanged",
+                "EventBusName": "PapaJohnsEventBus",
+                "Detail": json.dumps({
+                    "orderId": order_id,
+                    "status": new_status,
+                    "timestamp": timestamp
+                })
+            }
+        ]
+    )
+
+    return {
+        "statusCode": 200,
+        "body": json.dumps({
+            "order_id": order_id,
+            "status": new_status
+        })
+    }
