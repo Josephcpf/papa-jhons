3c6cb0e034d2b4598d639e18aab26f1a5f5e2c95
Josephcpf <joseph.cabanillas@utec.edu.pe>
2026-06-15 19:57:51 -0500

    Update getOrder.py

--- getOrder.py
+++ getOrder.py
@@ -1,20 +1,30 @@
-import boto3
+import json
 
-dynamodb = boto3.resource("dynamodb")
-table = dynamodb.Table("Orders")
+from src.shared.dynamodb import ORDERS_TABLE
+
 
 def handler(event, context):
 
-    order_id = event["pathParameters"]["id"]
+    order_id = event["pathParameters"]["orderId"]
 
-    response = table.get_item(
+    response = ORDERS_TABLE.get_item(
         Key={
             "tenant_id": "PAPAJOHNS",
             "order_id": order_id
         }
     )
 
+    item = response.get("Item")
+
+    if not item:
+        return {
+            "statusCode": 404,
+            "body": json.dumps({
+                "message": "Order not found"
+            })
+        }
+
     return {
         "statusCode": 200,
-        "body": response.get("Item", {})
+        "body": json.dumps(item)
     }
