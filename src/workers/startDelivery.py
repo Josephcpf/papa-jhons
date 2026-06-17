0e91a9212f75a9962ff4bf59b9ead7683a4b884a
Josephcpf <joseph.cabanillas@utec.edu.pe>
2026-06-17 12:22:06 -0500

    Update startDelivery.py

--- src/workers/startDelivery.py
+++ src/workers/startDelivery.py
@@ -1,30 +1,21 @@
 import json
 
-from src.shared.dynamodb import ORDERS_TABLE
+from src.shared.order_utils import update_order_status
 
 
 def handler(event, context):
 
     order_id = event["pathParameters"]["orderId"]
 
-    ORDERS_TABLE.update_item(
-        Key={
-            "tenant_id": "PAPAJOHNS",
-            "order_id": order_id
-        },
-        UpdateExpression="SET #status = :status",
-        ExpressionAttributeNames={
-            "#status": "status"
-        },
-        ExpressionAttributeValues={
-            ":status": "OUT_FOR_DELIVERY"
-        }
+    update_order_status(
+        order_id,
+        "READY_FOR_DELIVERY"
     )
 
     return {
         "statusCode": 200,
         "body": json.dumps({
             "order_id": order_id,
-            "status": "OUT_FOR_DELIVERY"
+            "status": "READY_FOR_DELIVERY"
         })
     }
