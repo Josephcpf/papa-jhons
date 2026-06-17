3b13deb2a777204aa4b68384a15e84137a1bd16c
Josephcpf <joseph.cabanillas@utec.edu.pe>
2026-06-17 12:19:10 -0500

    Update finishCooking.py

--- src/workers/finishCooking.py
+++ src/workers/finishCooking.py
@@ -1,30 +1,21 @@
 import json
 
-from src.shared.dynamodb import ORDERS_TABLE
+from src.shared.order_utils import update_order_status
 
 
 def handler(event, context):
 
     order_id = event["pathParameters"]["orderId"]
 
-    ORDERS_TABLE.update_item(
-        Key={
-            "tenant_id": "PAPAJOHNS",
-            "order_id": order_id
-        },
-        UpdateExpression="SET #status = :status",
-        ExpressionAttributeNames={
-            "#status": "status"
-        },
-        ExpressionAttributeValues={
-            ":status": "READY_FOR_PACKING"
-        }
+    update_order_status(
+        order_id,
+        "COOKING"
     )
 
     return {
         "statusCode": 200,
         "body": json.dumps({
             "order_id": order_id,
-            "status": "READY_FOR_PACKING"
+            "status": "COOKING"
         })
     }
