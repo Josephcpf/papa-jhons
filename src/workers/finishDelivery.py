62950540ed465399e61a4b7cee053132820332af
Josephcpf <joseph.cabanillas@utec.edu.pe>
2026-06-17 12:08:20 -0500

    Create finishDelivery.py

---
+++ src/workers/finishDelivery.py
@@ -0,0 +1,30 @@
+import json
+
+from src.shared.dynamodb import ORDERS_TABLE
+
+
+def handler(event, context):
+
+    order_id = event["pathParameters"]["orderId"]
+
+    ORDERS_TABLE.update_item(
+        Key={
+            "tenant_id": "PAPAJOHNS",
+            "order_id": order_id
+        },
+        UpdateExpression="SET #status = :status",
+        ExpressionAttributeNames={
+            "#status": "status"
+        },
+        ExpressionAttributeValues={
+            ":status": "DELIVERED"
+        }
+    )
+
+    return {
+        "statusCode": 200,
+        "body": json.dumps({
+            "order_id": order_id,
+            "status": "DELIVERED"
+        })
+    }
