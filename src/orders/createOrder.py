404a95f3ffc946c25d9a404c36995d055587d187
Josephcpf <joseph.cabanillas@utec.edu.pe>
2026-06-18 23:17:31 -0500

    Update createOrder.py

--- src/orders/createOrder.py
+++ src/orders/createOrder.py
@@ -7,12 +7,15 @@ from datetime import datetime
 dynamodb = boto3.resource("dynamodb")
 table = dynamodb.Table("Orders")
 
+events = boto3.client("events")
+
 
 def handler(event, context):
 
     body = json.loads(event["body"])
 
     order_id = str(uuid.uuid4())
+    timestamp = datetime.utcnow().isoformat()
 
     item = {
         "tenant_id": "PAPAJOHNS",
@@ -21,10 +24,27 @@ def handler(event, context):
         "status": "CREATED",
         "source": body["source"],
         "total": Decimal(str(body["total"])),
-        "created_at": datetime.utcnow().isoformat()
+        "created_at": timestamp
     }
 
     table.put_item(Item=item)
+
+    events.put_events(
+        Entries=[
+            {
+                "Source": "orders.service",
+                "DetailType": "OrderCreated",
+                "EventBusName": "PapaJohnsEventBus",
+                "Detail": json.dumps({
+                    "orderId": order_id,
+                    "customerId": body["customer_id"],
+                    "source": body["source"],
+                    "status": "CREATED",
+                    "timestamp": timestamp
+                })
+            }
+        ]
+    )
 
     return {
         "statusCode": 201,
