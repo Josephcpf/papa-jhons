826ff345c514173c50e3cfa763eddcac41695f32
Josephcpf <joseph.cabanillas@utec.edu.pe>
2026-06-18 19:51:32 -0500

    Update dynamodb.py

--- src/shared/dynamodb.py
+++ src/shared/dynamodb.py
@@ -4,3 +4,5 @@ dynamodb = boto3.resource("dynamodb")
 
 ORDERS_TABLE = dynamodb.Table("Orders")
 HISTORY_TABLE = dynamodb.Table("OrderHistory")
+WORKERS_TABLE = dynamodb.Table("Workers")
+TASKTOKENS_TABLE = dynamodb.Table("TaskTokens")
