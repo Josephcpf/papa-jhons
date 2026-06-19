9b92f4386840ec7e85f388c6b51ed5318f099751
Josephcpf <joseph.cabanillas@utec.edu.pe>
2026-06-18 23:07:42 -0500

    Update updateOrderStatus.py

--- src/orders/updateOrderStatus.py
+++ src/orders/updateOrderStatus.py
@@ -1,13 +1,6 @@
 import json
-import boto3
-from datetime import datetime
 
-from src.shared.dynamodb import (
-    ORDERS_TABLE,
-    HISTORY_TABLE
-)
-
-events = boto3.client("events")
+from src.shared.order_utils import update_order_status
 
 
 def handler(event, context):
@@ -15,53 +8,11 @@ def handler(event, context):
     order_id = event["pathParameters"]["orderId"]
 
     body = json.loads(event["body"])
-
     new_status = body["status"]
 
-    timestamp = datetime.utcnow().isoformat()
-
-    # Actualizar pedido
-    ORDERS_TABLE.update_item(
-        Key={
-            "tenant_id": "PAPAJOHNS",
-            "order_id": order_id
-        },
-        UpdateExpression="""
-            SET #status = :status,
-                updated_at = :updated_at
-        """,
-        ExpressionAttributeNames={
-            "#status": "status"
-        },
-        ExpressionAttributeValues={
-            ":status": new_status,
-            ":updated_at": timestamp
-        }
-    )
-
-    # Guardar historial
-    HISTORY_TABLE.put_item(
-        Item={
-            "order_id": order_id,
-            "event_time": timestamp,
-            "status": new_status
-        }
-    )
-
-    # Publicar evento en EventBridge
-    events.put_events(
-        Entries=[
-            {
-                "Source": "orders.service",
-                "DetailType": "OrderStatusChanged",
-                "EventBusName": "PapaJohnsEventBus",
-                "Detail": json.dumps({
-                    "orderId": order_id,
-                    "status": new_status,
-                    "timestamp": timestamp
-                })
-            }
-        ]
+    update_order_status(
+        order_id,
+        new_status
     )
 
     return {
@@ -70,4 +21,5 @@ def handler(event, context):
             "order_id": order_id,
             "status": new_status
         })
+    }
     }
